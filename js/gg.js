be914d55d5af992ee80b71e9de81731bdf8bc46d
ldat1 <lequocdat0933@gmail.com>
2024-11-13 00:21:13 -0300

    phuong

---
+++ js/gg.js
@@ -0,0 +1,6 @@
+
+    function handleCredentialResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+        // Bạn có thể giải mã JWT token này để lấy thông tin người dùng, hoặc gửi token này đến server để xác thực.
+    }
+
